8214928e2061fd83a675e39b6d3d3027b2b1636f
DavidABracer <davidbracer@yahoo.com>
2021-05-17 01:03:23 -0500

    Completed Lesson D Unit 5
    Finished Lesson D Unit 5

---
+++ Assets/Scripts/Difficulty8utton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Difficulty8utton : MonoBehaviour
+{
+    private Button button;
+    private GameManager gameManager;
+
+    public int difficulty;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(Setdifficulty);
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Setdifficulty()
+    {
+        gameManager.StartGame(difficulty); 
+    }
+}
